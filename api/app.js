712e5c5fa56658a1e1da0e9fe5d54223067c065c
AkashDhandhukiyA <161710123+AkashDhandhukiyA@users.noreply.github.com>
2026-01-10 14:07:52 +0530

    Changed

--- api/app.js
+++ api/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://todoo-tan.vercel.app/"
+      "https://todo-ten-sigma-34.vercel.app/"
     ],
     credentials: true,
   })
